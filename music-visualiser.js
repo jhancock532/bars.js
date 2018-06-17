9faa29a32f20c5101af235b14bd8225037307d5b
James Hancock <jhancock532@gmail.com>
2018-06-17 20:32:30 +0100

    Update music-visualiser.js

--- music-visualiser.js
+++ music-visualiser.js
@@ -5,7 +5,7 @@ function MusicVisualiser(numberOfBars, soundElementName){
   this.soundElementName = soundElementName;
   this.fftSize = 1024;
   this.warnNumSamplesAndFFTSizeAreInappropriate = function(fallthrough){
-    console.warn("EMV Warning : Reduced the number of bars to the max possible size for the current fftSize. (numberOfBars now equals "+fallthrough + ")");
+    console.warn("EMV Warning : Reduced the number of bars to the max possible size for the current fftSize. (numberOfBars now equals " + fallthrough + ")");
     return fallthrough;
   }
   this.numberOfBars = (numberOfBars < this.fftSize/4) ? numberOfBars : this.warnNumSamplesAndFFTSizeAreInappropriate(this.fftSize/4);
@@ -17,7 +17,7 @@ function MusicVisualiser(numberOfBars, soundElementName){
   this.soundDataArray;
   this.MAX_SOUND_VALUE = 256; 
   
-
+  //Loads a sound file from the users system, using a FileReader. Starts playing the music, and calls createAudioObjects.
   this.loadUserSelectedSoundFile = function(){
     let sound = document.getElementById(this.soundElementName);
     let reader = new FileReader(); 
@@ -30,6 +30,7 @@ function MusicVisualiser(numberOfBars, soundElementName){
     this.createAudioObjects();
   }
   
+  //Loads a sound file from the prespecified URL of the audio file (stored in this.mediaURL). Starts playing the music, and calls createAudioObjects.
   this.loadDefaultSoundFile = function(){
     let mediaUrl = this.mediaURL; 
     let sound = document.getElementById(this.soundElementName);
@@ -40,6 +41,7 @@ function MusicVisualiser(numberOfBars, soundElementName){
     this.createAudioObjects();
   }
   
+  //Connects all the WebAudioAPI objects together, and sets the settings of the analyser object.
   this.createAudioObjects = function(){
     let source = this.context.createMediaElementSource(document.getElementById(this.soundElementName));
     source.connect(this.analyser);
@@ -50,8 +52,8 @@ function MusicVisualiser(numberOfBars, soundElementName){
     this.soundDataArray = new Uint8Array(bufferLength);
   }
   
+  /* --- GET DATA FROM THE SOUND DATA ARRAY --- */
   //Returns the average of a small sample of the array. Index declares which sample you want, ideal for iteration.
-  //How to use this function is best described through example - see how I use it in the p5js code below.
   this.getSampleOfSoundData = function(index, noSampleSections){
     let sampleSize = Math.floor((this.soundDataArray.length/2) / noSampleSections); 
 
@@ -81,6 +83,35 @@ function MusicVisualiser(numberOfBars, soundElementName){
     }
   }
   
+  //Returns the average of the lowest 12th of the soundDataArray
+  this.getBass = function() {
+    if((this.soundDataArray === undefined) == false){
+      return this.getSampleOfSoundData(0,12); 
+    } else {
+      return 0;
+    }
+  }
+  
+  //Returns the middle third of the soundDataArray
+  this.getMidrange = function() {
+    if((this.soundDataArray === undefined) == false){
+      return this.getSampleOfSoundData(1,3);
+    } else {
+      return 0;
+    }
+  }
+  
+  //Returns the upper third of the soundDataArray
+  this.getTreble = function() {
+    if((this.soundDataArray === undefined) == false){
+      return this.getSampleOfSoundData(2,3);
+    } else {
+      return 0;
+    }
+  }
+  
+  /* --- UPDATE THE SOUND DATA AND BAR ARRAYS --- */
+  //Gets the latest frequency data into the soundDataArray and calls updateBars();
   this.updateFrequencyData = function(){
     //Be careful with trying to access an undefined array before music starts playing.
     if((this.soundDataArray === undefined) == false){
@@ -90,6 +121,7 @@ function MusicVisualiser(numberOfBars, soundElementName){
     this.updateBars();
   }
   
+  //Takes the data from soundDataArray and processes it into the bars array.
   this.updateBars = function(){
     let soundLevel = 0;
     for (let i = 0; i < this.numberOfBars; i++){
@@ -102,6 +134,8 @@ function MusicVisualiser(numberOfBars, soundElementName){
     }
   }
   
+  /* --- SET THE MUSIC VIS OPTIONS --- */
+  //Updates the size of the bars array.
   this.setNumberOfBars = function(numBars) {
     if (numBars > 0){
       this.numberOfBars = numBars;
@@ -118,14 +152,17 @@ function MusicVisualiser(numberOfBars, soundElementName){
     this.bars = Array(numBars).fill(0);
   }
   
+  //Updates the ID of what input element will be used to load the sound file.
   this.setAudioFileInputElementID = function(audioFileInputName){
     this.audioFileInputName = audioFileInputName;
   }
   
+  //Updates what audio file will be loaded by default, according to the URL of the file.
   this.setAudioFileURL = function(audioFileURL){
     this.mediaURL = audioFileURL;
   }
   
+  //Sets the fftSize (A power of 2) and verfies numberOfSamples is ok.
   this.setFFTSize = function(fftSize){
     let validfftSizeValues = [128, 256, 512, 1024, 2048];
     if (validfftSizeValues.includes(fftSize)){
@@ -145,6 +182,7 @@ function MusicVisualiser(numberOfBars, soundElementName){
 
   }
   
+  //Sets the smoothing factor of the analyser, checks that it is between 0 and 1.
   this.setSmoothingTimeConstant = function(smoothingConstant){
     if (smoothingConstant >= 0 && smoothingConstant <= 1){
       this.smoothingConstant = smoothingConstant;
@@ -156,3 +194,4 @@ function MusicVisualiser(numberOfBars, soundElementName){
       throw new Error("EMV - The smoothing constant should be a double between 0 and 1. No change has been made to the original smoothing constant, " +this.smoothingConstant);
   }
 }
+
